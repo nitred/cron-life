26a68dc2a419a58b0a522a429e299da0456a90c2
Nitish Reddy Koripalli <nitish.k.reddy@gmail.com>
2017-07-28 03:45:29 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup file for the package/project."""
+from setuptools import find_packages, setup
+
+setup(
+    name='cron-life',
+    version='0.0.1',
+    description='',
+    long_description='',
+    author='',
+    author_email='',
+    url='',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True)
